ada227a4b32e9e6c7d8ce2d5dce4ad57c3933808
Vitor Porto <45744241+vhpporto@users.noreply.github.com>
2021-11-08 16:33:52 -0300

    Update Packer.js
    ajuste packer offset

--- src/timeline/Packer.js
+++ src/timeline/Packer.js
@@ -1,15 +1,15 @@
 // @flow
 import XDate from 'xdate';
 
-const offset = 100;
+// const offset = 100;
 
-function buildEvent(column, left, width, dayStart) {
+function buildEvent(column, left, width, dayStart, offset) {
   const startTime = XDate(column.start);
   const endTime = column.end ? XDate(column.end) : XDate(startTime).addHours(1);
 
   const dayStartTime = XDate(startTime).clearTime();
-
   column.top = (dayStartTime.diffHours(startTime) - dayStart) * offset;
+  // console.log(column.title, column.start, offset)
   column.height = startTime.diffHours(endTime) * offset;
   column.width = width;
   column.left = left;
@@ -37,9 +37,10 @@ function expand(ev, column, columns) {
   return colSpan;
 }
 
-function pack(columns, width, calculatedEvents, dayStart) {
+function pack(columns, width, calculatedEvents, dayStart, offset) {
   let colLength = columns.length;
 
+  // console.log(offset)
   for (let i = 0; i < colLength; i++) {
     let col = columns[i];
     for (let j = 0; j < col.length; j++) {
@@ -47,12 +48,13 @@ function pack(columns, width, calculatedEvents, dayStart) {
       const L = (i / colLength) * width;
       const W = (width * colSpan) / colLength - 10;
 
-      calculatedEvents.push(buildEvent(col[j], L, W, dayStart));
+      calculatedEvents.push(buildEvent(col[j], L, W, dayStart, offset));
     }
   }
 }
 
-function populateEvents(events, screenWidth, dayStart) {
+function populateEvents(events, screenWidth, dayStart, offset) {
+  console.log(offset)
   let lastEnd;
   let columns;
   let calculatedEvents = [];
@@ -72,7 +74,7 @@ function populateEvents(events, screenWidth, dayStart) {
 
   events.forEach(function (ev) {
     if (lastEnd !== null && ev.start >= lastEnd) {
-      pack(columns, screenWidth, calculatedEvents, dayStart);
+      pack(columns, screenWidth, calculatedEvents, dayStart, offset);
       columns = [];
       lastEnd = null;
     }
@@ -97,7 +99,7 @@ function populateEvents(events, screenWidth, dayStart) {
   });
 
   if (columns.length > 0) {
-    pack(columns, screenWidth, calculatedEvents, dayStart);
+    pack(columns, screenWidth, calculatedEvents, dayStart,offset);
   }
   return calculatedEvents;
 }

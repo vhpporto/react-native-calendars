6c478dd9f7110abc9130800f70bdf0517f014178
Vitor Porto <45744241+vhpporto@users.noreply.github.com>
2023-02-24 11:30:36 -0300

    Update Timeline.js
    refactor/quebrar linha descrição evento

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -12,22 +12,23 @@ import styleConstructor from './style';
 
 const LEFT_MARGIN = 60 - 1;
 const TEXT_LINE_HEIGHT = 17;
+const HORA_CHEIA_EM_MINUTOS = 60;
 
-function calculaOffset(param = 15)  {
+function calculaOffset(param = 15) {
   if (param == 10) {
-    return 200
-  } 
+    return 215
+  }
   if (param == 15 || param == 45) {
-    return 140
-  } 
-    return 130
+    return 155
+  }
+  return 155
 }
 
 function range(from, to) {
   return Array.from(Array(to), (_, i) => from + i);
 }
 
-let {width: dimensionWidth} = Dimensions.get('window');
+let { width: dimensionWidth } = Dimensions.get('window');
 
 export default class Timeline extends React.PureComponent {
   static propTypes = {
@@ -43,10 +44,10 @@ export default class Timeline extends React.PureComponent {
         // summary: PropTypes.strinyarn g.isRequired,
         color: PropTypes.string,
       })
-      ).isRequired,
-      scrollToNow: PropTypes.bool,
-      currentDateString: PropTypes.string,
-      updateCurrentTimeIndicatorEveryMinute: PropTypes.bool,
+    ).isRequired,
+    scrollToNow: PropTypes.bool,
+    currentDateString: PropTypes.string,
+    updateCurrentTimeIndicatorEveryMinute: PropTypes.bool,
   };
 
   static defaultProps = {
@@ -59,7 +60,7 @@ export default class Timeline extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    const {start, end, paramTempoAgenda} = this.props;
+    const { start, end, paramTempoAgenda } = this.props;
     this.calendarHeight = (end - start) * calculaOffset(paramTempoAgenda)
     this.style = styleConstructor(props.styles, this.calendarHeight);
 
@@ -75,8 +76,8 @@ export default class Timeline extends React.PureComponent {
 
   componentDidUpdate(prevProps) {
     const width = dimensionWidth - LEFT_MARGIN;
-    const {events: prevEvents, start: prevStart = 0, paramTempoAgenda: prevParam} = prevProps;
-    const {events, end = 24, start = 0, paramTempoAgenda} = this.props;
+    const { events: prevEvents, start: prevStart = 0, paramTempoAgenda: prevParam } = prevProps;
+    const { events, end = 24, start = 0, paramTempoAgenda } = this.props;
 
     if (prevEvents !== events || prevStart !== start || prevParam !== paramTempoAgenda) {
       this.calendarHeight = (end - start) * calculaOffset(paramTempoAgenda)
@@ -128,7 +129,7 @@ export default class Timeline extends React.PureComponent {
   }
 
   currentTimeOffset() {
-    const {start = 0, paramTempoAgenda} = this.props;
+    const { start = 0, paramTempoAgenda } = this.props;
     const offset = calculaOffset(paramTempoAgenda);
     const timeNowHour = moment().hour();
     const timeNowMin = moment().minutes();
@@ -136,7 +137,7 @@ export default class Timeline extends React.PureComponent {
     return offset * (timeNowHour - start) + (offset * timeNowMin) / 60;
   }
 
-  isCurrentDateStringForTimeIndicatorSet () {
+  isCurrentDateStringForTimeIndicatorSet() {
     return typeof this.props.currentDateString !== 'undefined';
   }
 
@@ -144,27 +145,27 @@ export default class Timeline extends React.PureComponent {
     if (this.isCurrentDateStringForTimeIndicatorSet() &&
       this.props.currentDateString === moment().format('YYYY-MM-DD')) {
       return (
-        <View style={{flexDirection: 'row'}}>
-          <View style={[{top: this.state.currentTimeIndicatorTopCoordinate}, this.style.containerTime]}>
-              <Text style={this.style.textTimeNow}>{moment(new Date()).format('HH:mm')}</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={[{ top: this.state.currentTimeIndicatorTopCoordinate }, this.style.containerTime]}>
+            <Text style={this.style.textTimeNow}>{moment(new Date()).format('HH:mm')}</Text>
           </View>
 
-        <View
-          key={'timeNow'}
-          style={[
-            this.style.lineNow,
-            {
-              top: this.state.currentTimeIndicatorTopCoordinate,
-              width: dimensionWidth - 20,
-            },
-          ]}
+          <View
+            key={'timeNow'}
+            style={[
+              this.style.lineNow,
+              {
+                top: this.state.currentTimeIndicatorTopCoordinate,
+                width: dimensionWidth - 20,
+              },
+            ]}
           />
-          </View>
+        </View>
       );
     }
   }
 
-  
+
   scrollToFirst() {
     setTimeout(() => {
       if (this.state && this.state._scrollY && this._scrollView) {
@@ -179,22 +180,22 @@ export default class Timeline extends React.PureComponent {
 
 
   _renderLines() {
-    const {format24h, start = 0, end = 24, paramTempoAgenda  = 30} = this.props;
+    const { format24h, start = 0, end = 24, paramTempoAgenda = 30 } = this.props;
     let param = paramTempoAgenda
     if (paramTempoAgenda == 40) {
-      param = 20 
+      param = 20
     } else if (paramTempoAgenda == 45) {
-      param = 15 
+      param = 15
     } else if (paramTempoAgenda == 60) {
-      param = 30 
+      param = 30
     }
-    const HORA_CHEIA_EM_MINUTOS = 60;
+
     let QUANTIDADE_SLOTS = Math.ceil(HORA_CHEIA_EM_MINUTOS / param)
     const TAMANHO_SLOT_HORA = this.calendarHeight / 24 / (60 / param)
     const EVENT_DIFF = 20;
     const offset = this.calendarHeight / (end - start);
 
-    
+
 
     return range(start, end + 1).map((hora, index) => {
       let timeText;
@@ -212,30 +213,30 @@ export default class Timeline extends React.PureComponent {
       }
 
       return [
-        <Text key={`timeLabel${hora}`} style={[this.style.timeLabel, {top: offset * index - 6}]}>
+        <Text key={`timeLabel${hora}`} style={[this.style.timeLabel, { top: offset * index - 6 }]}>
           {timeText}
         </Text>,
         [...Array(QUANTIDADE_SLOTS).keys()].map((slot, indx) => (
           <Pressable
             activeOpacity={1}
             key={`bt1line${indx}`}
-            onPress={() => { this.props.toggleModal(hora * HORA_CHEIA_EM_MINUTOS + ( HORA_CHEIA_EM_MINUTOS / QUANTIDADE_SLOTS * indx) )}}
+            onPress={() => { this.props.toggleModal(hora * HORA_CHEIA_EM_MINUTOS + (HORA_CHEIA_EM_MINUTOS / QUANTIDADE_SLOTS * indx)) }}
             style={({ pressed }) => [
-              this.style.line, 
-              {backgroundColor: pressed ? COLORS.secondary : COLORS.white},
-              {height: TAMANHO_SLOT_HORA, top: offset * index + (indx * TAMANHO_SLOT_HORA), width: dimensionWidth - EVENT_DIFF}]}
+              this.style.line,
+              { backgroundColor: pressed ? COLORS.secondary : COLORS.white },
+              { height: TAMANHO_SLOT_HORA, top: offset * index + (indx * TAMANHO_SLOT_HORA), width: dimensionWidth - EVENT_DIFF }]}
           />
         ))
-        ]
+      ]
     });
   }
 
-  renderAlertAusente(title, subtitle,id, citCodigo) {
+  renderAlertAusente(title, subtitle, id, citCodigo) {
     return Alert.alert(title, subtitle, [
       {
         text: 'Não',
       },
-      { 
+      {
         text: 'Sim',
         onPress: () => this.props.atualizaComanda(id, citCodigo)
       }
@@ -258,12 +259,12 @@ export default class Timeline extends React.PureComponent {
     ])
   }
 
-  renderAlert(title, subtitle,id) {
+  renderAlert(title, subtitle, id) {
     return Alert.alert(title, subtitle, [
       {
         text: 'Não',
       },
-      { 
+      {
         text: 'Sim',
         onPress: () => this.props.desbloquearHorario(id)
       }
@@ -301,19 +302,70 @@ export default class Timeline extends React.PureComponent {
     this.props.abrirDetalhesAgendamento(JSON.stringify(event))
   }
 
-  horaAgendamento(inicio,fim, isAssinatura) {
+  horaAgendamento(inicio, fim, isAssinatura) {
     const formatTime = this.props.format24h ? 'HH:mm' : 'hh:mm A';
     return (
       <>
-        {isAssinatura && <FontAwesome name='star'  color='orange'/>  }{' '}
-        {XDate(inicio).toString(formatTime)} - {XDate(fim).toString(formatTime)}{' '}
+        <Text style={[this.style.eventTitle]}>
+          <Text style={[this.style.eventTimes, this.style.bold]}>
+            {isAssinatura && <FontAwesome name='star' color='orange' />}{' '}
+            {XDate(inicio).toString(formatTime)} - {XDate(fim).toString(formatTime)}{' '}
+          </Text>
+        </Text>
       </>
 
     )
   }
 
+  _renderBloqueio(event, titleFontSize) {
+    return (
+      <Text style={[this.style.eventTitle, { fontSize: titleFontSize }]}>
+        {this.horaAgendamento(event.start, event.end)}Agenda bloqueada
+      </Text>
+    )
+  }
+
+  _renderSemJornada(event) {
+    return this.horaAgendamento(event.start, event.end)
+  }
+
+  _renderEventUntillTwoLines(event, numberOfLines, titleFontSize, obsFontSize, isAssinatura, isAgendamento) {
+    if (!isAgendamento) return null
+    return (
+      <Text numberOfLines={numberOfLines} style={[[this.style.eventTitle, { fontSize: titleFontSize }]]}>
+        {this.horaAgendamento(event.start, event.end, isAssinatura)}
+        {' '}{event.servico || ''} - {event.usuario}  <Text numberOfLines={numberOfLines - 1} style={[this.style.eventSummary, { fontSize: obsFontSize }]}>
+          {event.obs !== 'Agenda Bloqueada' && event.obs || ''}
+        </Text>
+      </Text>
+    )
+  }
+
+  _renderEventThreeLines(event, numberOfLines, titleFontSize, obsFontSize, isAgendamento) {
+    if (!isAgendamento) return null
+    return (
+      <Text numberOfLines={numberOfLines - 1} style={[this.style.eventTitle, { fontSize: titleFontSize }]}>
+        {event.servico || ''} - {isAgendamento && event.usuario}
+        <Text numberOfLines={numberOfLines} style={[this.style.eventSummary, { fontSize: obsFontSize, fontWeight: 'normal' }]}>
+          {' '}{event.obs !== 'Agenda Bloqueada' && event.obs || ''}
+        </Text>
+      </Text>
+    )
+  }
+
+  _renderEventBiggerThreeLines(event, numberOfLines, titleFontSize, obsFontSize, isAgendamento) {
+    return (
+      <>
+        <Text numberOfLines={numberOfLines} style={[this.style.eventTitle, { fontSize: titleFontSize }]}>
+          {event.servico || ''} - {isAgendamento && event.usuario}
+        </Text>
+        <Text numberOfLines={numberOfLines - 1} style={[this.style.eventSummary, { fontSize: obsFontSize }]}>{event.obs !== 'Agenda Bloqueada' && event.obs || ''}</Text>
+      </>
+    )
+  }
+
   _renderEvents() {
-    const {packedEvents} = this.state;
+    const { packedEvents } = this.state;
     let events = packedEvents.map((event, i) => {
       const style = {
         left: event.left,
@@ -321,61 +373,46 @@ export default class Timeline extends React.PureComponent {
         width: event.width,
         top: event.top,
         borderLeftWidth: 2,
-        backgroundColor: event.color ? event.color  : '#bfe0ff'
+        backgroundColor: event.color ? event.color : '#bfe0ff'
       };
+
+      const isBloqueio = event.status === 'Bloqueado'
+      const isSemJornada = event.status === 'SEM JORNADA'
+      const isAgendamento = !isBloqueio && !isSemJornada
 
       const numberOfLines = Math.floor(event.height / TEXT_LINE_HEIGHT);
       const isAssinatura = event.assinatura && event.assinatura !== '';
+      const obsFontSize = numberOfLines <= 3 ? 10 : 11
+      const titleFontSize = numberOfLines <= 3 ? 12 : 14
 
       return (
         <TouchableOpacity
           activeOpacity={0.9}
           onPress={() => this._onEventTapped(this.props.events[event.index])}
           key={i}
-          style={[this.style.event, style]}
-        >
+          style={[this.style.event, style]}>
           {this.props.renderEvent ? (
             this.props.renderEvent(event)
           ) : (
             <View>
-              {event.status === 'Bloqueado' && (
-                <Text numberOfLines={2} style={this.style.eventTitle}>
-                 Agenda bloqueada
-              </Text>
-              ) }
-              {event.title === 'SEM JORNADA' && (
-                <Text numberOfLines={2} style={this.style.eventTitle}>
-                 <Text style={[this.style.eventTimes, this.style.bold]}>
-                  {this.horaAgendamento(event.start, event.end)}
-                 </Text>
-              </Text>
-              ) }
+              {isBloqueio ? this._renderBloqueio(event, titleFontSize) : null}
+              {isSemJornada ? this._renderSemJornada(event) : null}
+
               {numberOfLines <= 2 ? (
-               event.title !== 'SEM JORNADA' && event.status !== 'Bloqueado' &&
-                <Text numberOfLines={2} style={this.style.eventTitle}>
-                  <Text style={[this.style.eventTimes, this.style.bold]}>
-                    {this.horaAgendamento(event.start, event.end, isAssinatura)}</Text>
-                    {' '}{event.servico || ''} - {event.usuario}
-                </Text>
+                this._renderEventUntillTwoLines(event, numberOfLines, titleFontSize, obsFontSize, isAssinatura, isAgendamento)
               ) : (
                 <>
-                  {event.title !== 'SEM JORNADA' && (
-                  <Text style={[this.style.eventTimes, this.style.bold]}>
-                  {this.horaAgendamento(event.start, event.end, isAssinatura)}
-                  </Text>
+                  {isAgendamento && (
+                    this.horaAgendamento(event.start, event.end, isAssinatura)
                   )}
-                  <Text numberOfLines={2} style={this.style.eventTitle}>
-                    {event.servico || ''} - {event.status !== 'Bloqueado' && event.status !== 'SEM JORNADA' && event.usuario }
-                  </Text>
+                  {numberOfLines > 3 ? (
+                    this._renderEventBiggerThreeLines(event, numberOfLines, titleFontSize, obsFontSize, isAgendamento)
+                  ) : (
+                    this._renderEventThreeLines(event, numberOfLines, titleFontSize, obsFontSize, isAgendamento)
+                  )}
                 </>
               )}
-              {numberOfLines >= 1 &&  event.title !== 'SEM JORNADA' && event.status !== 'Bloqueado' &&
-                (
-                  <Text numberOfLines={numberOfLines - 1} style={[this.style.eventSummary]}>
-                    {event.obs !== 'Agenda Bloqueada' && event.obs || ''}
-                  </Text>
-                )
-              }              
+
             </View>
           )}
         </TouchableOpacity>
@@ -384,7 +421,7 @@ export default class Timeline extends React.PureComponent {
 
     return (
       <View>
-        <View style={{marginLeft: LEFT_MARGIN}}>{events}</View>
+        <View style={{ marginLeft: LEFT_MARGIN }}>{events}</View>
       </View>
     );
   }
@@ -392,7 +429,7 @@ export default class Timeline extends React.PureComponent {
   render() {
     return (
       <ScrollView
-         refreshControl={
+        refreshControl={
           <RefreshControl
             refreshing={this.props.loading}
             onRefresh={this.props.buscaJornadaProfissional}
@@ -400,7 +437,7 @@ export default class Timeline extends React.PureComponent {
         }
         ref={ref => (this._scrollView = ref)}
         contentContainerStyle={[this.style.contentStyle,
-        {width: dimensionWidth}]}>
+        { width: dimensionWidth }]}>
         {this._renderLines()}
         {this._renderEvents()}
         {this._renderCurrentTimeIndicator()}
